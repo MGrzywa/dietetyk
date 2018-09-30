dfbf0807d78d6495bd7166d0a51626af404b454e
MGrzywa <maciekgrzywa@gmail.com>
2018-09-30 19:10:31 +0200

    project init

---
+++ models/ingredient.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const ingredient = sequelize.define('ingredient', {
+    name: DataTypes.STRING
+  }, {});
+  ingredient.associate = function(models) {
+    // associations can be defined here
+  };
+  return ingredient;
+};
